23c402127af23204860ae783d0c7734c51f488f4
Dimentorr <94733623+Dimentorr@users.noreply.github.com>
2023-11-13 10:24:48 +1000

    add admin menu and calculate currency

--- main.py
+++ main.py
@@ -3,12 +3,34 @@ import dotenv
 
 import telebot
 from telebot import types
+import requests
 
-dotenv.load_dotenv()
+import random
+
+
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
 
 bot = telebot.TeleBot(os.getenv('TOKEN', 'your token'))
 
+tag = ''
 
+
+#
+def update_env(key, new_value):
+    os.environ[f'{key}'] = f'{new_value}'
+
+    dotenv.set_key(dotenv_file, f'{key}', os.environ[f'{key}'])
+
+
+#
+def binance_rub(rub):
+    binanceTick1 = requests.get("https://api.binance.com/api/v3/ticker/price?symbol=BTCRUB")
+    rub_in_btc = binanceTick1.json()['price']
+    return (rub * 100) / round(float(rub_in_btc), 8)
+
+
+#
 def order_step(message):
     if message.text == 'В начало':
         start_menu(message)
@@ -22,6 +44,7 @@ def order_step(message):
     bot.register_next_step_handler(message, city_step, order_data=order_data)
 
 
+#
 def city_step(message, order_data):
     if message.text == 'В начало':
         start_menu(message)
@@ -33,6 +56,7 @@ def city_step(message, order_data):
     bot.register_next_step_handler(message, district_step, order_data=order_data)
 
 
+#
 def district_step(message, order_data):
     if message.text == 'В начало':
         start_menu(message)
@@ -57,6 +81,7 @@ def product_step(message, order_data):
     bot.register_next_step_handler(message, count_step, order_data=order_data)
 
 
+#
 def count_step(message, order_data):
     if message.text == 'В начало':
         start_menu(message)
@@ -70,13 +95,16 @@ def count_step(message, order_data):
     bot.register_next_step_handler(message, operator_step, order_data=order_data)
 
 
+#
 def operator_step(message, order_data):
     if message.text == 'В начало':
         start_menu(message)
         return
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     construction_keyboard_product(keyboard, env_name='')
-    price = os.getenv('PRICE').split('/')
+    price = os.getenv('PRICE')
+
+    currency = os.getenv('CURRENCY').split('/')
     order_data['get'] = message.text
     bot.send_message(
         message.from_user.id,
@@ -86,7 +114,7 @@ Id заказа - {order_data['id']}
 Район - {order_data['district']}
 Заказ - {order_data['count']} {order_data['product']}
 Способ получение - {order_data['get']}
-Стоимость - {float(price[0]) * float(order_data['count'])}/{float(price[1]) * float(order_data['count'])}
+Стоимость - {float(price) * float(order_data['count'])} {currency[0]}/{binance_rub(float(price)) * float(order_data['count'])} {currency[1]}
 Ожидайте ответа оператора
             ''',
         reply_markup=keyboard
@@ -97,11 +125,12 @@ Id заказа - {order_data['id']}
 Район - {order_data['district']}
 Заказ - {order_data['count']} {order_data['product']}
 Способ получение - {order_data['get']}
-Стоимость - {float(price[0]) * float(order_data['count'])}/{float(price[1]) * float(order_data['count'])}
+Стоимость - {float(price) * float(order_data['count'])} {currency[0]}/{binance_rub(float(price)) * float(order_data['count'])} {currency[1]}
 Логин пользователя - @{message.from_user.username}
             ''')
 
 
+#
 def construction_keyboard_product(keyboard, env_name):
     key_back = types.InlineKeyboardButton(text='В начало', callback_data='start')
     keyboard.add(key_back)
@@ -111,12 +140,56 @@ def construction_keyboard_product(keyboard, env_name):
             keyboard.add(key)
 
 
+#
+def keyboard_admin(keyboard):
+    key_back = types.InlineKeyboardButton(text='В начало', callback_data='back')
+    keyboard.add(key_back)
+    # --------------------------------------------------------------------------------
+    key_pass = types.InlineKeyboardButton(text='Пароль', callback_data='pass')
+    # keyboard.add(key_pass)
+    # --------------------------------------------------------------------------------
+    key_city = types.InlineKeyboardButton(text='Город', callback_data='city')
+    # keyboard.row(key_city)
+    # --------------------------------------------------------------------------------
+    key_district = types.InlineKeyboardButton(text='Район', callback_data='district')
+    # keyboard.add(key_district)
+    # --------------------------------------------------------------------------------
+    key_product = types.InlineKeyboardButton(text='Товар', callback_data='product')
+    # keyboard.add(key_product)
+    # --------------------------------------------------------------------------------
+    key_sell = types.InlineKeyboardButton(text='Стоимость', callback_data='sell')
+    # keyboard.add(key_sell)
+    # --------------------------------------------------------------------------------
+    key_count = types.InlineKeyboardButton(text='Количество', callback_data='count')
+    # keyboard.add(key_count)
+    keyboard.row(key_city, key_district)
+    keyboard.row(key_product, key_count, key_sell)
+    keyboard.add(key_pass)
+
+
+#
+def keyboard_password_admin(keyboard):
+    key_back = types.InlineKeyboardButton(text='В начало', callback_data='back')
+    keyboard.add(key_back)
+
+
+#
+def keyboard_admin_update_data(keyboard):
+    key_back = types.InlineKeyboardButton(text='В начало', callback_data='start')
+    keyboard.add(key_back)
+    # --------------------------------------------------------------------------------
+    key_back_step = types.InlineKeyboardButton(text='Назад к выбору', callback_data='back')
+    keyboard.add(key_back_step)
+
+
+#
 def send_image(message, name_img):
     photo = open(f'img/{name_img}', 'rb')
     bot.send_photo(message.from_user.id, photo)
     photo.close()
 
 
+#
 def help_menu(message):
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     construction_keyboard_product(keyboard, '')
@@ -126,6 +199,7 @@ def help_menu(message):
 ''', reply_markup=keyboard)
 
 
+#
 def start_menu(message):
     # send_image(message, name_img='name')
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -137,12 +211,102 @@ def start_menu(message):
                      f'''Привет, я бот''', reply_markup=keyboard)
 
 
+#
+def password_before_admin_menu(message):
+    if message.text == 'В начало':
+        start_menu(message)
+        return
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard_password_admin(keyboard)
+    bot.send_message(message.from_user.id, f'''Введите пароль:''', reply_markup=keyboard)
+    bot.register_next_step_handler(message, admin_menu)
+
+
+#
+def admin_menu(message, password=False):
+    if message.text == 'В начало':
+        start_menu(message)
+        return
+    if message.text == os.getenv('PASSWORD') or password:
+        bot.send_message(message.from_user.id, f'''Админ панель
+<-------------------------->''')
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard_admin(keyboard)
+
+        bot.send_message(message.from_user.id, f'''Выберите необходимый пункт''', reply_markup=keyboard)
+        bot.register_next_step_handler(message, intermediate_pass_stape)
+    else:
+        bot.send_message(message.from_user.id, f'''Неверный пароль''')
+        bot.register_next_step_handler(message, password_before_admin_menu)
+
+
+# сохраняем изменения в .env
+def invisible_update_step(message, data_name):
+    if message.text == 'В начало':
+        start_menu(message)
+        return
+    elif message.text == 'Назад к выбору':
+        admin_menu(message, password=True)
+        return
+    bot.send_message(message.from_user.id, f'''Изменения применены''')
+    update_env(data_name, message.text)
+    bot.register_next_step_handler(message, admin_menu)
+
+
+# data_name - это ключ для .env
+def update_step(message, data_name):
+    if message.text == 'В начало':
+        start_menu(message)
+        return
+    elif message.text == 'Назад к выбору':
+        admin_menu(message, password=True)
+        return
+    bot.send_message(message.from_user.id, f'''Введите новое значение
+<-------------------------->''')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard_admin_update_data(keyboard)
+    if message.text == 'PRICE':
+        bot.send_message(message.from_user.id, f'''Доступные товары {os.getenv("PRODUCTS")}''')
+
+    bot.send_message(message.from_user.id,
+                     f'''Текущее значение переменной {data_name}: {os.getenv(data_name)}
+При необходимости добавления нескольких пунктов используете "," без использования пробелов
+''', reply_markup=keyboard)
+    bot.register_next_step_handler(message, invisible_update_step, data_name=data_name)
+
+
+# Передаём дальше необходимый для работы с .env ключ
+def intermediate_pass_stape(message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    key_back = types.InlineKeyboardButton(text='Продолжить', callback_data='next')
+    keyboard.add(key_back)
+    if message.text == 'В начало':
+        start_menu(message)
+        return
+    bot.send_message(message.from_user.id, f'''Вынужденная пауза
+<-------------------------->''', reply_markup=keyboard)
+    if message.text == 'Пароль':
+        bot.register_next_step_handler(message, update_step, data_name='PASSWORD')
+    elif message.text == 'Стоимость':
+        bot.send_message(message.from_user.id, f'''Доступные товары {os.getenv("PRODUCTS")}''')
+        bot.register_next_step_handler(message, update_step, data_name='PRICE')
+    elif message.text == 'Товар':
+        bot.register_next_step_handler(message, update_step, data_name='PRODUCTS')
+    elif message.text == 'Город':
+        bot.register_next_step_handler(message, update_step, data_name='CITIES')
+    elif message.text == 'Район':
+        bot.register_next_step_handler(message, update_step, data_name='DISTRICTS')
+    elif message.text == 'Количество':
+        bot.register_next_step_handler(message, update_step, data_name='COUNT')
+
+
+#
 @bot.message_handler(content_types=['text'])
 def callback_inline(call):
     if call.text == 'В начало' or call.text == '/start':
         start_menu(call)
     elif call.text == 'Сделать заказ':
-        id_order = os.getenv('NUM_ORDER')
+        id_order = random.randint(100000, 1000000)
         order_data = {'id': id_order}
         os.environ['NUM_ORDER'] = str(int(id_order) + 1)
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -153,6 +317,8 @@ def callback_inline(call):
         help_menu(call)
     elif call.text == '/ID':
         bot.send_message(call.chat.id, f'Chat id = {call.chat.id}')
+    elif call.text == '/admin':
+        password_before_admin_menu(call)
 
 
 bot.polling(none_stop=True, interval=0)
